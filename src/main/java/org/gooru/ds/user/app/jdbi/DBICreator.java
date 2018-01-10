e06c910704acfe2e7c1bfd6eac6328b17e545d1c
am-gooru <ashish@gooru.org>
2018-01-10 10:49:49 +0530

    Include more infrastructure

---
+++ src/main/java/org/gooru/ds/user/app/jdbi/DBICreator.java
@@ -0,0 +1,29 @@
+package org.gooru.ds.user.app.jdbi;
+
+import javax.sql.DataSource;
+
+import org.gooru.ds.user.app.components.DataSourceRegistry;
+import org.skife.jdbi.v2.DBI;
+
+/**
+ * @author ashish on 1/10/18.
+ */
+public final class DBICreator {
+
+    private DBICreator() {
+        throw new AssertionError();
+    }
+
+    public static DBI getDbiForDefaultDS() {
+        return createDBI(DataSourceRegistry.getInstance().getDefaultDataSource());
+    }
+
+    private static DBI createDBI(DataSource dataSource) {
+        DBI dbi = new DBI(dataSource);
+        dbi.registerArgumentFactory(new PostgresIntegerArrayArgumentFactory());
+        dbi.registerArgumentFactory(new PostgresStringArrayArgumentFactory());
+        dbi.registerArgumentFactory(new PostgresUUIDArrayArgumentFactory());
+        return dbi;
+    }
+
+}
