27c3132a8a8ef487231601a48e3d77b48897b2d6
am-gooru <ashish@gooru.org>
2018-01-11 10:30:10 +0530

    Make distribution consider the duration as well

--- sql/pre-baseline.sql
+++ sql/pre-baseline.sql
@@ -2,34 +2,40 @@
 -- Once we have baselined it, we can move it to baseline script which could be run with flyway
 
 -- drop table user_distribution_zoom1
+-- drop table user_distribution_subject
 -- drop table user_distribution_zoom1_subject
 
 CREATE TABLE user_distribution_zoom1 (
     id bigserial PRIMARY KEY,
-    code character varying(512) NOT NULL UNIQUE,
+    code character varying(512) NOT NULL,
+    duration character varying(512) NOT NULL,
     name character varying(512) NOT NULL,
     total bigint NOT NULL DEFAULT 0,
     active bigint NOT NULL DEFAULT 0,
-    updated_at timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL
+    updated_at timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL,
+    UNIQUE (code, duration)
 );
 
 CREATE TABLE user_distribution_subject (
     id bigserial PRIMARY KEY,
     code character varying(512) NOT NULL UNIQUE,
+    duration character varying(512) NOT NULL,
     name character varying(512) NOT NULL,
     total bigint NOT NULL DEFAULT 0,
     active bigint NOT NULL DEFAULT 0,
-    updated_at timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL
+    updated_at timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL,
+    UNIQUE (code, duration)
 );
 
 CREATE TABLE user_distribution_zoom1_subject (
     id bigserial PRIMARY KEY,
     code character varying(512) NOT NULL,
+    duration character varying(512) NOT NULL,
     name character varying(512) NOT NULL,
     subject_code character varying(512) NOT NULL,
     subject_name character varying(512) NOT NULL,
     total bigint NOT NULL DEFAULT 0,
     active bigint NOT NULL DEFAULT 0,
     updated_at timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL,
-    UNIQUE (code, subject_code)
+    UNIQUE (code, subject_code, duration)
 );
--- src/main/java/org/gooru/ds/user/processor/user/distribution/UserDistributionDao.java
+++ src/main/java/org/gooru/ds/user/processor/user/distribution/UserDistributionDao.java
@@ -12,23 +12,24 @@ import org.skife.jdbi.v2.sqlobject.customizers.Mapper;
 interface UserDistributionDao {
 
     @Mapper(UserDistributionModelMapper.class)
-    @SqlQuery("select code, name, total, active from user_distribution_zoom1")
+    @SqlQuery("select code, name, total, active from user_distribution_zoom1 where duration = :activeDuration")
     List<UserDistributionModel> fetchUserDistributionAcrossGeoLocations(
-        UserDistributionCommand.UserDistributionCommandBean userDistributionCommandBean);
+        @BindBean UserDistributionCommand.UserDistributionCommandBean userDistributionCommandBean);
 
     @Mapper(UserDistributionModelMapper.class)
-    @SqlQuery("select code, name, total, active from user_distribution_subject")
+    @SqlQuery("select code, name, total, active from user_distribution_subject where duration = :activeDuration")
     List<UserDistributionModel> fetchUserDistributionAcrossSubjects(
-        UserDistributionCommand.UserDistributionCommandBean userDistributionCommandBean);
+        @BindBean UserDistributionCommand.UserDistributionCommandBean userDistributionCommandBean);
 
     @Mapper(UserDistributionModelMapper.class)
-    @SqlQuery("select code, name, active, total from user_distribution_zoom1_subject where subject_code = :subject")
+    @SqlQuery("select code, name, active, total from user_distribution_zoom1_subject where subject_code = :subject "
+                  + " and duration = :activeDuration")
     List<UserDistributionModel> fetchUserDistributionBySubjectAcrossGeoLocations(
         @BindBean UserDistributionCommand.UserDistributionCommandBean userDistributionCommandBean);
 
     @Mapper(UserDistributionModelMapper.class)
     @SqlQuery("select subject_code as code, subject_name as name, active, total from user_distribution_zoom1_subject "
-                  + "where code = :geoLocation")
+                  + "where code = :geoLocation and duration = :activeDuration")
     List<UserDistributionModel> fetchUserDistributionByGeoLocationAcrossSubjects(
         @BindBean UserDistributionCommand.UserDistributionCommandBean userDistributionCommandBean);
 }
