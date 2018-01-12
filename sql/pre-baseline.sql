dfde704dee0af518edf50995c5444c1df91edcd3
gooru <mukul@gooru.org>
2018-01-12 12:19:50 +0530

    Schema for Master Data housing and Content Level Aggregated Views/Tables

--- sql/pre-baseline.sql
+++ sql/pre-baseline.sql
@@ -7,6 +7,13 @@
 -- drop table active_user_stats
 -- drop table user_profile_master
 -- drop table user_grade_map
+-- drop table ds_master
+-- drop table resource_timespent
+-- drop table course_collection_performance
+-- drop table overall_course_performance
+-- drop table user_courses
+-- drop table course_performance
+
 
 CREATE TABLE user_distribution_zoom1 (
     id bigserial PRIMARY KEY,
@@ -104,3 +111,143 @@ CREATE TABLE user_vectors (
     reputation integer,
     updated_at timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL
 );
+
+
+-- This is the master data table that forms the leaf level data housing 
+-- Aggregated Tables will be created/derived from this master tables.
+CREATE TABLE ds_master (
+id BIGSERIAL PRIMARY KEY,
+event_name varchar(36),
+event_type varchar(36),
+tenant_id varchar(36),
+actor_id varchar(36),
+class_id varchar(36),
+class_title varchar(100),
+class_code varchar(7),
+course_id varchar(36),
+course_title varchar(100),
+unit_id varchar(36),
+unit_title varchar(100),
+lesson_id varchar(36),
+lesson_title varchar(100),
+collection_id varchar(36),
+collection_title varchar(36),
+session_id varchar(36),
+question_count smallint,
+collection_type varchar(12),
+resource_type varchar(12),
+resource__content_type varchar(20),
+question_type varchar(36),
+answer_object text,
+resource_id varchar(36),
+resource_title varchar(100),
+time_spent bigint,
+views integer,
+reaction smallint,
+resource_attempt_status varchar(10),
+score numeric(5,2),
+max_score numeric(5,2),
+grading_type varchar(10),
+app_id varchar,
+partner_id varchar(36),
+collection_sub_type varchar(36),
+path_id bigint,
+event_id varchar(36),
+time_zone varchar(50),
+content_source varchar(36),
+date_in_time_zone date,
+is_graded boolean,
+created_at timestamp NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
+updated_at timestamp NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'));
+
+
+-- Resource TimeSpent based on the resource_content_type
+CREATE TABLE resource_timespent (
+id SERIAL PRIMARY KEY,
+user_id varchar(36),
+resource__content_type varchar(20),
+resource_id varchar(36),
+resource_title varchar(100),
+resource_time_spent bigint,
+created_at timestamp NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
+updated_at timestamp NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'));
+
+
+CREATE TABLE overall_course_performance (
+id SERIAL PRIMARY KEY,
+user_id varchar(36),
+class_id varchar(36),
+class_title varchar(100),
+class_code varchar(7),
+course_id varchar(36),
+course_title varchar(100),
+assessments_completed bigint,
+total_assessments bigint,
+time_spent bigint,
+average_score numeric(5,2),
+created_at timestamp NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
+updated_at timestamp NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'));
+
+CREATE TABLE course_performance (
+id SERIAL PRIMARY KEY,
+user_id varchar(36),
+class_id varchar(36),
+class_title varchar(100),
+class_code varchar(7),
+course_id varchar(36),
+course_title varchar(100),
+course_asmt_time_spent bigint,
+course_asmt_score numeric(5,2),
+course_coll_time_spent bigint,
+course_coll_score numeric(5,2),
+unit_id varchar(36),
+unit_title varchar(100),
+unit_asmt_time_spent bigint,
+unit_asmt_score numeric(5,2),
+unit_coll_time_spent bigint,
+unit_coll_score numeric(5,2),
+lesson_id varchar(36),
+lesson_title varchar(100),
+lesson_asmt_time_spent bigint,
+lesson_asmt_score numeric(5,2),
+lesson_coll_time_spent bigint,
+lesson_coll_score numeric(5,2),
+assessments_completed bigint,
+total_assessments bigint,
+created_at timestamp NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
+updated_at timestamp NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'));
+
+
+CREATE TABLE course_collection_performance (
+id SERIAL PRIMARY KEY,
+user_id varchar(36),
+class_id varchar(36),
+class_title varchar(100),
+class_code varchar(7),
+course_id varchar(36),
+course_title varchar(100),
+unit_id varchar(36),
+unit_title varchar(100),
+lesson_id varchar(36),
+lesson_title varchar(100),
+collection_id varchar(36),
+collection_title varchar(100),
+collection_time_spent bigint,
+collection_score numeric(5,2),
+collection_average_reaction smallint,
+collection_num_attempts smallint,
+collection_type varchar(12),
+created_at timestamp NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
+updated_at timestamp NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'));
+
+CREATE TABLE user_courses (
+id BIGSERIAL PRIMARY KEY,
+user_id varchar(36),
+in_class_course_id varchar(36),
+in_class_course_title varchar(100),
+class_code varchar(7),
+il_course_id varchar(36),
+il_course_title varchar(100),
+created_at timestamp NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
+updated_at timestamp NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'));
+
