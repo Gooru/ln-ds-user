c2ac7bffdca2f05a9414d5c0bbcc5c9f5d1a446e
gooru <mukul@gooru.org>
2018-01-29 20:09:24 +0530

    update tablename for summary report api

--- src/main/java/org/gooru/ds/user/processor/userperf/summary/assessment/UserPerfAsmtSummaryDao.java
+++ src/main/java/org/gooru/ds/user/processor/userperf/summary/assessment/UserPerfAsmtSummaryDao.java
@@ -12,7 +12,7 @@ import org.skife.jdbi.v2.sqlobject.customizers.Mapper;
 interface UserPerfAsmtSummaryDao {
 
     @Mapper(UserPerfAsmtSummaryModelMapper.class)
-    @SqlQuery("select resource_id, score, reaction, time_spent, resource_type, path_id " + "from base_reports "
+    @SqlQuery("select resource_id, score, reaction, time_spent, resource_type, path_id " + " from ds_master "
                   + "where actor_id = :user and ((class_id IS NULL AND :classId IS NULL) OR class_id = :classId) "
                   + "and course_id = :courseId and lesson_id = :lessonId "
                   + "and unit_id = :unitId and collection_id = :assessmentId and session_id = :sessionId "
