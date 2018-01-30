9237fdeee9182f0efc701d49124b85cbaa939367
am-gooru <ashish@gooru.org>
2018-01-30 18:00:24 +0530

    Authority needs to be scaled down by 1000 for profile API

--- src/main/java/org/gooru/ds/user/processor/userprofile/UserProfileDao.java
+++ src/main/java/org/gooru/ds/user/processor/userprofile/UserProfileDao.java
@@ -12,7 +12,7 @@ interface UserProfileDao {
     @Mapper(UserProfileModelMapper.class)
     @SqlQuery("select upm.user_id, upm.username, upm.email, upm.first_name, upm.last_name, upm.user_category, upm"
                   + ".tenant_id, upm.thumbnail, upm.gender, upm.about, upm.school, upm.school_district, upm.country, "
-                  + "upm.state, uv.authority/1000::real, authority, uv.reputation/1000::real "
+                  + "upm.state, uv.authority/1000::real authority, uv.reputation/1000::real "
                   + "reputation, uv.citizenship/1000::real citizenship  from users_profile_master upm, user_vectors  "
                   + "uv where upm.user_id = :user and upm.user_id = uv.user_id")
     UserProfileModel fetchUserProfile(@BindBean UserProfileCommand.UserProfileCommandBean userProfileCommandBean);
