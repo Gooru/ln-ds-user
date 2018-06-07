050f4bd3ae2a84187ca0e073a1969e602ecb4fda
szgooru <sachin@gooru.org>
2018-06-07 13:12:03 +0530

    updating the query to remove non existing columns

--- src/main/java/org/gooru/ds/user/processor/userperf/summary/assessment/UserPerfAsmtSummaryDao.java
+++ src/main/java/org/gooru/ds/user/processor/userperf/summary/assessment/UserPerfAsmtSummaryDao.java
@@ -12,7 +12,7 @@ import org.skife.jdbi.v2.sqlobject.customizers.Mapper;
 interface UserPerfAsmtSummaryDao {
 
     @Mapper(UserPerfAsmtSummaryModelMapper.class)
-    @SqlQuery("select resource_id, score, reaction, time_spent, resource_type, path_id, resource_title, resource_content_type " 
+    @SqlQuery("select resource_id, score, reaction, time_spent, resource_type, path_id " 
                   + "from base_reports where actor_id = :user and ((class_id IS NULL AND :classId IS NULL) OR class_id = :classId) "
                   + "and course_id = :courseId and lesson_id = :lessonId "
                   + "and unit_id = :unitId and collection_id = :assessmentId and session_id = :sessionId "
@@ -21,7 +21,7 @@ interface UserPerfAsmtSummaryDao {
         @BindBean UserPerfAsmtSummaryCommand.UserPerfAsmtSummaryCommandBean userPerfAsmtSummaryCommandBean);
 
     @Mapper(UserPerfAsmtSummaryModelMapper.class)
-    @SqlQuery("select resource_id, score, reaction, time_spent, resource_type, path_id, resource_title, resource_content_type " 
+    @SqlQuery("select resource_id, score, reaction, time_spent, resource_type, path_id " 
                   + "from base_reports where actor_id = :user and collection_id = :assessmentId and session_id = :sessionId "
                   + "and event_name = 'collection.resource.play' and event_type = 'stop'")
     List<UserPerfAsmtSummaryModel> fetchUserPerfAsmtSummaryforComp(
