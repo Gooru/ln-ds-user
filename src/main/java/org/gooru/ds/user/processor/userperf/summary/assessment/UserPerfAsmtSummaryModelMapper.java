a827fa38590a33d0405b81db6bf3f76d0c5e0a0d
szgooru <sachin@gooru.org>
2018-06-07 13:18:02 +0530

    updating model mapper to remove the non existing columns

--- src/main/java/org/gooru/ds/user/processor/userperf/summary/assessment/UserPerfAsmtSummaryModelMapper.java
+++ src/main/java/org/gooru/ds/user/processor/userperf/summary/assessment/UserPerfAsmtSummaryModelMapper.java
@@ -15,9 +15,7 @@ public class UserPerfAsmtSummaryModelMapper implements ResultSetMapper<UserPerfA
     public UserPerfAsmtSummaryModel map(int index, ResultSet r, StatementContext ctx) throws SQLException {
         UserPerfAsmtSummaryModel model = new UserPerfAsmtSummaryModel();
         model.setId(r.getString(MapperFields.RESOURCE_ID));
-        model.setTitle(r.getString(MapperFields.RESOURCE_TITLE));
         model.setType(r.getString(MapperFields.RESOURCE_TYPE));
-        model.setContentType(r.getString(MapperFields.RESOURCE_CONTENT_TYPE));
         model.setTimeSpent(r.getLong(MapperFields.RESOURCE_TIMESPENT));
         model.setScore(r.getDouble(MapperFields.RESOURCE_SCORE));
         model.setReaction(r.getInt(MapperFields.RESOURCE_REACTION));
@@ -33,9 +31,7 @@ public class UserPerfAsmtSummaryModelMapper implements ResultSetMapper<UserPerfA
         }
 
         private static final String RESOURCE_ID = "resource_id";
-        private static final String RESOURCE_TITLE = "resource_title";
         private static final String RESOURCE_TYPE = "resource_type";
-        private static final String RESOURCE_CONTENT_TYPE = "resource_content_type";
         private static final String RESOURCE_TIMESPENT = "time_spent";
         private static final String RESOURCE_SCORE = "score";
         private static final String RESOURCE_REACTION = "reaction";
