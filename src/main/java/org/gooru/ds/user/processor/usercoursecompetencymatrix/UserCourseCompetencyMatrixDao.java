d2c9f585c3ac9271e7a1d67e6eb33594a61f131b
Mukul P Markande <mukul@gooru.org>
2018-07-06 12:52:52 +0530

    NILE-2786: No Data seen on the Proficiency Tab

--- src/main/java/org/gooru/ds/user/processor/usercoursecompetencymatrix/UserCourseCompetencyMatrixDao.java
+++ src/main/java/org/gooru/ds/user/processor/usercoursecompetencymatrix/UserCourseCompetencyMatrixDao.java
@@ -25,15 +25,16 @@ interface UserCourseCompetencyMatrixDao {
 	@SqlQuery("SELECT MAX(updated_at) FROM learner_profile_competency_status WHERE user_id = :userId AND tx_subject_code = :subjectCode")
 	Timestamp fetchLastUpdatedTime(@Bind("userId") String userId, @Bind("subjectCode") String subjectCode);
 	
-    @Mapper(UserCourseCompetencyMatrixModelMapper.class)
-    @SqlQuery("select distinct(cm.tx_comp_code), cm.tx_course_code, cm.tx_comp_name, cm.tx_comp_desc, cm.tx_comp_student_desc, cm.tx_comp_seq,"
-    		+ " (SELECT DISTINCT ON (lpcs.gut_code) FIRST_VALUE(lpcs.status) OVER (PARTITION BY lpcs.gut_code ORDER BY lpcs.updated_at desc) as"
-    		+ " status FROM learner_profile_competency_status_ts lpcs where lpcs.gut_code = ucm.gut_code and extract(month from lpcs.updated_at)"
-    		+ " <= :month and extract(year from lpcs.updated_at) <= :year) as status from course_competency_matrix cm left"
-    		+ " join learner_profile_competency_status_ts ucm on  cm.tx_subject_code = ucm.tx_subject_code and cm.tx_comp_code = ucm.gut_code and"
-    		+ " ucm.user_id = :user and extract(month from ucm.updated_at) <= :month AND extract(year from updated_at) <= :year where"
-    		+ " cm.tx_subject_code = :subject order by cm.tx_course_code, cm.tx_comp_seq asc")
-    List<UserCourseCompetencyMatrixModel> fetchUserCourseCompetencyMatrixTillMonth(@BindBean
-        UserCourseCompetencyMatrixCommand.UserCourseCompetencyMatrixCommandBean userCompetencyMatrixCommandBean);
+	  //Commented due to NILE-2786
+//    @Mapper(UserCourseCompetencyMatrixModelMapper.class)
+//    @SqlQuery("select distinct(cm.tx_comp_code), cm.tx_course_code, cm.tx_comp_name, cm.tx_comp_desc, cm.tx_comp_student_desc, cm.tx_comp_seq,"
+//    		+ " (SELECT DISTINCT ON (lpcs.gut_code) FIRST_VALUE(lpcs.status) OVER (PARTITION BY lpcs.gut_code ORDER BY lpcs.updated_at desc) as"
+//    		+ " status FROM learner_profile_competency_status_ts lpcs where lpcs.gut_code = ucm.gut_code and extract(month from lpcs.updated_at)"
+//    		+ " <= :month and extract(year from lpcs.updated_at) <= :year) as status from course_competency_matrix cm left"
+//    		+ " join learner_profile_competency_status_ts ucm on  cm.tx_subject_code = ucm.tx_subject_code and cm.tx_comp_code = ucm.gut_code and"
+//    		+ " ucm.user_id = :user and extract(month from ucm.updated_at) <= :month AND extract(year from updated_at) <= :year where"
+//    		+ " cm.tx_subject_code = :subject order by cm.tx_course_code, cm.tx_comp_seq asc")
+//    List<UserCourseCompetencyMatrixModel> fetchUserCourseCompetencyMatrixTillMonth(@BindBean
+//        UserCourseCompetencyMatrixCommand.UserCourseCompetencyMatrixCommandBean userCompetencyMatrixCommandBean);
 
 }
--- src/main/java/org/gooru/ds/user/processor/userdomaincompetencymatrix/UserDomainCompetencyMatrixDao.java
+++ src/main/java/org/gooru/ds/user/processor/userdomaincompetencymatrix/UserDomainCompetencyMatrixDao.java
@@ -23,16 +23,17 @@ interface UserDomainCompetencyMatrixDao {
 	
 	@SqlQuery("SELECT MAX(updated_at) FROM learner_profile_competency_status WHERE user_id = :userId AND tx_subject_code = :subjectCode")
 	Timestamp fetchLastUpdatedTime(@Bind("userId") String userId, @Bind("subjectCode") String subjectCode);
-	
-	@Mapper(UserDomainCompetencyMatrixModelMapper.class)
-	@SqlQuery("select distinct(cm.tx_comp_code), cm.tx_domain_code, cm.tx_comp_name, cm.tx_comp_desc, cm.tx_comp_student_desc, cm.tx_comp_seq,"
-			+ " (SELECT DISTINCT ON (lpcs.gut_code) FIRST_VALUE(lpcs.status) OVER (PARTITION BY lpcs.gut_code ORDER BY lpcs.updated_at desc) as status"
-			+ " FROM learner_profile_competency_status_ts lpcs where lpcs.gut_code = ucm.gut_code and extract(month from lpcs.updated_at) <= :month"
-			+ " and extract(year from lpcs.updated_at) <= :year) as status from domain_competency_matrix cm left join learner_profile_competency_status_ts"
-			+ " ucm on cm.tx_subject_code = ucm.tx_subject_code and cm.tx_comp_code = ucm.gut_code and ucm.user_id = :user and extract(month from"
-			+ " ucm.updated_at) <= :month and extract(year from ucm.updated_at) <= :year where cm.tx_subject_code = :subject order by cm.tx_domain_code,"
-			+ " cm.tx_comp_seq asc")
-	List<UserDomainCompetencyMatrixModel> fetchUserDomainCompetencyMatrixTillMonth(
-			@BindBean UserDomainCompetencyMatrixCommand.UserDomainCompetencyMatrixCommandBean userCompetencyMatrixCommandBean);
+
+	//Commented due to NILE-2786
+//	@Mapper(UserDomainCompetencyMatrixModelMapper.class)
+//	@SqlQuery("select distinct(cm.tx_comp_code), cm.tx_domain_code, cm.tx_comp_name, cm.tx_comp_desc, cm.tx_comp_student_desc, cm.tx_comp_seq,"
+//			+ " (SELECT DISTINCT ON (lpcs.gut_code) FIRST_VALUE(lpcs.status) OVER (PARTITION BY lpcs.gut_code ORDER BY lpcs.updated_at desc) as status"
+//			+ " FROM learner_profile_competency_status_ts lpcs where lpcs.gut_code = ucm.gut_code and extract(month from lpcs.updated_at) <= :month"
+//			+ " and extract(year from lpcs.updated_at) <= :year) as status from domain_competency_matrix cm left join learner_profile_competency_status_ts"
+//			+ " ucm on cm.tx_subject_code = ucm.tx_subject_code and cm.tx_comp_code = ucm.gut_code and ucm.user_id = :user and extract(month from"
+//			+ " ucm.updated_at) <= :month and extract(year from ucm.updated_at) <= :year where cm.tx_subject_code = :subject order by cm.tx_domain_code,"
+//			+ " cm.tx_comp_seq asc")
+//	List<UserDomainCompetencyMatrixModel> fetchUserDomainCompetencyMatrixTillMonth(
+//			@BindBean UserDomainCompetencyMatrixCommand.UserDomainCompetencyMatrixCommandBean userCompetencyMatrixCommandBean);
 
 }
