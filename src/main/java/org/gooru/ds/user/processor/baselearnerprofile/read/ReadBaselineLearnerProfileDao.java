cadd16eef2872aa3cea06945bef5d41ada82022a
Renu <100067@bq-067.bambeeq.com>
2018-07-30 15:43:30 +0530

    Removed unnecessary join

--- src/main/java/org/gooru/ds/user/processor/baselearnerprofile/read/ReadBaselineLearnerProfileDao.java
+++ src/main/java/org/gooru/ds/user/processor/baselearnerprofile/read/ReadBaselineLearnerProfileDao.java
@@ -28,17 +28,13 @@ public interface ReadBaselineLearnerProfileDao {
 	List<ReadBaselineLearnerProfileModel> readBaselineLearnerProfileIL(
 			@BindBean ReadBaselineLearnerProfileCommand.ReadBaselineLearnerProfileCommandBean bean);
 	
-    @SqlQuery("SELECT MAX(blp.created_at) FROM "
-            + "domain_competency_matrix cm LEFT JOIN baseline_learner_profile blp ON cm.tx_subject_code = blp.tx_subject_code AND "
-            + "cm.tx_comp_code = blp.gut_code AND blp.class_id = :classId AND blp.course_id = :courseId AND blp.user_id = :user WHERE "
-            + "cm.tx_subject_code = :subject")
+	@SqlQuery("SELECT MAX(created_at) FROM baseline_learner_profile "
+        + "WHERE class_id = :classId AND course_id = :courseId AND user_id = :user AND tx_subject_code = :subject")
 	Timestamp fetchLastCreatedTimeInClass(
-            @BindBean ReadBaselineLearnerProfileCommand.ReadBaselineLearnerProfileCommandBean bean);
-
-	@SqlQuery("SELECT MAX(blp.created_at) FROM "
-	    + "domain_competency_matrix cm LEFT JOIN baseline_learner_profile blp ON cm.tx_subject_code = blp.tx_subject_code AND "
-	    + "cm.tx_comp_code = blp.gut_code AND blp.course_id = :courseId AND blp.user_id = :user WHERE "
-	    + "cm.tx_subject_code = :subject")
+        @BindBean ReadBaselineLearnerProfileCommand.ReadBaselineLearnerProfileCommandBean bean);
+	
+	@SqlQuery("SELECT MAX(created_at) FROM baseline_learner_profile "
+	    + "WHERE course_id = :courseId AND user_id = :user AND  tx_subject_code = :subject")
 	Timestamp fetchLastCreatedTimeIL(
             @BindBean ReadBaselineLearnerProfileCommand.ReadBaselineLearnerProfileCommandBean bean);
 	
