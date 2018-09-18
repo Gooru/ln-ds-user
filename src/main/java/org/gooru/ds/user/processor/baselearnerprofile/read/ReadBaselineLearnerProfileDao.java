53790af6f0e7798fb777fe682b3e8fe0e2cdc925
szgooru <sachin@gooru.org>
2018-09-18 16:01:42 +0530

    fixing read baseline profile IL queries to explicity filter on null class

--- src/main/java/org/gooru/ds/user/processor/baselearnerprofile/read/ReadBaselineLearnerProfileDao.java
+++ src/main/java/org/gooru/ds/user/processor/baselearnerprofile/read/ReadBaselineLearnerProfileDao.java
@@ -23,7 +23,7 @@ public interface ReadBaselineLearnerProfileDao {
 	@Mapper(ReadBaselineLearnerProfileModelMapper.class)
 	@SqlQuery("SELECT cm.tx_domain_code, cm.tx_comp_code, cm.tx_comp_name, cm.tx_comp_desc, cm.tx_comp_student_desc, cm.tx_comp_seq, blp.status FROM"
 			+ " domain_competency_matrix cm LEFT JOIN baseline_learner_profile blp ON cm.tx_subject_code = blp.tx_subject_code AND "
-			+ " cm.tx_comp_code = blp.gut_code AND blp.course_id = :courseId AND blp.user_id = :user WHERE"
+			+ " cm.tx_comp_code = blp.gut_code AND blp.class_id IS NULL AND blp.course_id = :courseId AND blp.user_id = :user WHERE"
 			+ " cm.tx_subject_code = :subject ORDER BY cm.tx_domain_code, cm.tx_comp_seq ASC")
 	List<ReadBaselineLearnerProfileModel> readBaselineLearnerProfileIL(
 			@BindBean ReadBaselineLearnerProfileCommand.ReadBaselineLearnerProfileCommandBean bean);
@@ -34,7 +34,7 @@ public interface ReadBaselineLearnerProfileDao {
         @BindBean ReadBaselineLearnerProfileCommand.ReadBaselineLearnerProfileCommandBean bean);
 	
 	@SqlQuery("SELECT MAX(created_at) FROM baseline_learner_profile "
-	    + "WHERE course_id = :courseId AND user_id = :user AND  tx_subject_code = :subject")
+	    + "WHERE course_id = :courseId AND user_id = :user AND class_id IS NULL AND tx_subject_code = :subject")
 	Timestamp fetchLastCreatedTimeIL(
             @BindBean ReadBaselineLearnerProfileCommand.ReadBaselineLearnerProfileCommandBean bean);
 	
