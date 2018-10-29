741a54701eb646ff078c9f8931120f87f7569e36
am-gooru <ashish@gooru.org>
2018-10-29 10:18:10 +0530

    LP Baseline read API should work off new table. The new table has cached value which can be returned as is instead of old table which requires a lot of calculation to be done

--- src/main/java/org/gooru/ds/user/processor/baselearnerprofile/read/ReadBaselineLearnerProfileProcessor.java
+++ src/main/java/org/gooru/ds/user/processor/baselearnerprofile/read/ReadBaselineLearnerProfileProcessor.java
@@ -1,5 +1,9 @@
 package org.gooru.ds.user.processor.baselearnerprofile.read;
 
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.json.JsonObject;
 import org.gooru.ds.user.app.data.EventBusMessage;
 import org.gooru.ds.user.app.jdbi.DBICreator;
 import org.gooru.ds.user.processor.MessageProcessor;
@@ -8,63 +12,40 @@ import org.gooru.ds.user.responses.MessageResponseFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import io.vertx.core.Future;
-import io.vertx.core.Vertx;
-import io.vertx.core.eventbus.Message;
-import io.vertx.core.json.DecodeException;
-import io.vertx.core.json.JsonObject;
-
-/**
- * @author szgooru on 20-Jul-2018
- */
 public class ReadBaselineLearnerProfileProcessor implements MessageProcessor {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(ReadBaselineLearnerProfileProcessor.class);
-	private final Vertx vertx;
-	private final Message<JsonObject> message;
-	private final Future<MessageResponse> result;
-	private EventBusMessage eventBusMessage;
+  private static final Logger LOGGER = LoggerFactory
+      .getLogger(ReadBaselineLearnerProfileProcessor.class);
+  private final Vertx vertx;
+  private final Message<JsonObject> message;
+  private final Future<MessageResponse> result;
+  private EventBusMessage eventBusMessage;
 
-	private final ReadBaselineLearnerProfileService service = new ReadBaselineLearnerProfileService(
-			DBICreator.getDbiForDefaultDS());
+  private final ReadBaselineLearnerProfileService service = new ReadBaselineLearnerProfileService(
+      DBICreator.getDbiForDefaultDS());
 
-	public ReadBaselineLearnerProfileProcessor(Vertx vertx, Message<JsonObject> message) {
-		this.vertx = vertx;
-		this.message = message;
-		this.result = Future.future();
-	}
+  public ReadBaselineLearnerProfileProcessor(Vertx vertx, Message<JsonObject> message) {
+    this.vertx = vertx;
+    this.message = message;
+    this.result = Future.future();
+  }
 
-	@Override
-	public Future<MessageResponse> process() {
-		try {
-			this.eventBusMessage = EventBusMessage.eventBusMessageBuilder(message);
-			ReadBaselineLearnerProfileCommand command = ReadBaselineLearnerProfileCommand
-					.build(this.eventBusMessage.getRequestBody());
-			readBaselineLearnerProfile(command);
-		} catch (Throwable throwable) {
-			LOGGER.warn("Encountered exception", throwable);
-			result.fail(throwable);
-		}
-		return result;
-	}
+  @Override
+  public Future<MessageResponse> process() {
+    try {
+      this.eventBusMessage = EventBusMessage.eventBusMessageBuilder(message);
+      ReadBaselineLearnerProfileCommand command = ReadBaselineLearnerProfileCommand
+          .build(this.eventBusMessage.getRequestBody());
+      readBaselineLearnerProfile(command);
+    } catch (Throwable throwable) {
+      LOGGER.warn("Encountered exception", throwable);
+      result.fail(throwable);
+    }
+    return result;
+  }
 
-	private void readBaselineLearnerProfile(ReadBaselineLearnerProfileCommand command) {
-		try {
-			ReadBaselineLearnerProfileModelResponse outcome = this.service.fetchBaselineLearnerProfile(command);
-			String resultString = new ObjectMapper().writeValueAsString(outcome);
-			result.complete(MessageResponseFactory.createOkayResponse(new JsonObject(resultString)));
-		} catch (JsonProcessingException e) {
-			LOGGER.error("Not able to convert data to JSON", e);
-			result.fail(e);
-		} catch (DecodeException e) {
-			LOGGER.warn("Not able to convert data to JSON", e);
-			result.fail(e);
-		} catch (Throwable throwable) {
-			LOGGER.warn("Encountered exception", throwable);
-			result.fail(throwable);
-		}
-	}
+  private void readBaselineLearnerProfile(ReadBaselineLearnerProfileCommand command) {
+    String outcome = this.service.fetchBaselineLearnerProfile(command);
+    result.complete(MessageResponseFactory.createOkayResponse(new JsonObject(outcome)));
+  }
 }
--- src/main/java/org/gooru/ds/user/processor/baselearnerprofile/read/ReadBaselineLearnerProfileService.java
+++ src/main/java/org/gooru/ds/user/processor/baselearnerprofile/read/ReadBaselineLearnerProfileService.java
@@ -1,92 +1,27 @@
 package org.gooru.ds.user.processor.baselearnerprofile.read;
 
-import java.sql.Timestamp;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import org.gooru.ds.user.constants.StatusConstants;
 import org.skife.jdbi.v2.DBI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * @author szgooru on 20-Jul-2018
- */
-public class ReadBaselineLearnerProfileService {
+class ReadBaselineLearnerProfileService {
 
-	private final static Logger LOGGER = LoggerFactory.getLogger(ReadBaselineLearnerProfileService.class);
+  private final static Logger LOGGER = LoggerFactory
+      .getLogger(ReadBaselineLearnerProfileService.class);
 
-	private final ReadBaselineLearnerProfileDao dao;
+  private final ReadBaselineLearnerProfileDao dao;
 
-	public ReadBaselineLearnerProfileService(DBI dbi) {
-		this.dao = dbi.onDemand(ReadBaselineLearnerProfileDao.class);
-	}
+  ReadBaselineLearnerProfileService(DBI dbi) {
+    this.dao = dbi.onDemand(ReadBaselineLearnerProfileDao.class);
+  }
 
-	public ReadBaselineLearnerProfileModelResponse fetchBaselineLearnerProfile(
-			ReadBaselineLearnerProfileCommand command) {
+  String fetchBaselineLearnerProfile(
+      ReadBaselineLearnerProfileCommand command) {
+    if (command.getClassId() != null) {
+      return dao.fetchProfileBaselinedForUserInClass(command.asBean());
+    } else {
+      return dao.fetchProfileBaselinedForUserInIL(command.asBean());
+    }
+  }
 
-		List<ReadBaselineLearnerProfileModel> models = null;
-		
-		Timestamp lastCreated = null;
-		ReadBaselineLearnerProfileCommand.ReadBaselineLearnerProfileCommandBean bean = command.asBean(command);
-		if (bean.getSubject() == null) {
-			LOGGER.debug("no subject found, returning empty response");
-			return new ReadBaselineLearnerProfileModelResponse();
-		}
-		
-		if (command.getClassId() != null && !command.getClassId().isEmpty()) {
-			models = this.dao.readBaselineLearnerProfile(bean);
-			lastCreated = this.dao.fetchLastCreatedTimeInClass(bean);
-		} else {
-			models = this.dao.readBaselineLearnerProfileIL(bean);
-			lastCreated = this.dao.fetchLastCreatedTimeIL(bean);
-		}
-
-		if (models == null || models.isEmpty()) {
-			LOGGER.debug("no records returned, returning empty");
-			return new ReadBaselineLearnerProfileModelResponse();
-		}
-
-		List<ReadBaselineLearnerProfileModel> completed = models.stream()
-				.filter(model -> model.getStatus() >= StatusConstants.COMPLETED).collect(Collectors.toList());
-
-		Map<String, Map<String, ReadBaselineLearnerProfileModel>> completedCompMatrixMap = new HashMap<>();
-		completed.forEach(model -> {
-			String domain = model.getDomainCode();
-			String compCode = model.getCompetencyCode();
-
-			if (completedCompMatrixMap.containsKey(domain)) {
-				Map<String, ReadBaselineLearnerProfileModel> competencies = completedCompMatrixMap.get(domain);
-				competencies.put(compCode, model);
-				completedCompMatrixMap.put(domain, competencies);
-			} else {
-				Map<String, ReadBaselineLearnerProfileModel> competencies = new HashMap<>();
-				competencies.put(compCode, model);
-				completedCompMatrixMap.put(domain, competencies);
-			}
-		});
-
-		models.forEach(model -> {
-			String domainCode = model.getDomainCode();
-			int sequence = model.getCompetencySeq();
-			int status = model.getStatus();
-
-			if (completedCompMatrixMap.containsKey(domainCode)) {
-				Map<String, ReadBaselineLearnerProfileModel> competencies = completedCompMatrixMap.get(domainCode);
-				for (Map.Entry<String, ReadBaselineLearnerProfileModel> entry : competencies.entrySet()) {
-					ReadBaselineLearnerProfileModel compModel = entry.getValue();
-					int compSeq = compModel.getCompetencySeq();
-
-					if (sequence < compSeq && status < StatusConstants.ASSERTED) {
-						model.setStatus(StatusConstants.INFERRED);
-					}
-				}
-			}
-		});
-
-		return ReadBaselineLearnerProfileModelResponseBuilder.build(models, lastCreated);
-
-	}
 }
