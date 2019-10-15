a22739b928bd379e2002d68440341361eefd5b0a
RenukaGurumurthy <renuka.g@bambeeq.com>
2019-10-15 23:39:17 +0530

    portfolio: query improvement to support inclusive date range.

--- src/main/java/org/gooru/ds/user/processor/user/portfolio/domain/UserDomainPortfolioDao.java
+++ src/main/java/org/gooru/ds/user/processor/user/portfolio/domain/UserDomainPortfolioDao.java
@@ -19,7 +19,7 @@ interface UserDomainPortfolioDao {
 
   @Mapper(UserDomainPortfolioModelMapper.class)
   @SqlQuery("select gut_code, collection_id, latest_session_id, collection_score, collection_type, class_id, course_id, unit_id, lesson_id, content_source, created_at, updated_at "
-      + " from learner_profile_competency_evidence where collection_type IN ('assessment', 'assessment-external') AND gut_code = ANY(:gutCode) and user_id = :userId AND updated_at <= :dateUntil "
+      + " from learner_profile_competency_evidence where collection_type IN ('assessment', 'assessment-external') AND gut_code = ANY(:gutCode) and user_id = :userId AND CAST(updated_at AS DATE) <= :dateUntil "
       + " order by updated_at desc OFFSET :offset LIMIT :limit")
   List<UserDomainPortfolioModel> fetchCompetencyAssessments(
       @BindBean UserDomainPortfolioCommand.UserDomainPortfolioCommandBean userDomainPortfolioCommandBean,
@@ -28,14 +28,14 @@ interface UserDomainPortfolioDao {
   @Mapper(UserDomainPortfolioModelMapper.class)
   @SqlQuery("select gut_code, collection_id, latest_session_id, collection_score, collection_type, class_id, course_id, unit_id, lesson_id, content_source, created_at, updated_at "
       + " from learner_profile_competency_evidence where collection_type IN ('assessment', 'assessment-external') AND gut_code = ANY(:gutCode) and user_id = :userId "
-      + " AND updated_at BETWEEN :startDate AND :endDate order by updated_at desc OFFSET :offset LIMIT :limit")
+      + " AND CAST(updated_at AS DATE) BETWEEN :startDate AND :endDate order by updated_at desc OFFSET :offset LIMIT :limit")
   List<UserDomainPortfolioModel> fetchCompetencyAssessmentsInDateRange(
       @BindBean UserDomainPortfolioCommand.UserDomainPortfolioCommandBean userDomainPortfolioCommandBean,
       @Bind("gutCode") PGArray<String> gutCode);
 
   @Mapper(UserDomainPortfolioModelMapper.class)
   @SqlQuery("select gut_code, collection_id, latest_session_id, collection_score, collection_type, class_id, course_id, unit_id, lesson_id, content_source, created_at, updated_at "
-      + " from learner_profile_competency_evidence where collection_type IN ('collection', 'collection-external') AND gut_code = ANY(:gutCode) and user_id = :userId AND updated_at <= :dateUntil "
+      + " from learner_profile_competency_evidence where collection_type IN ('collection', 'collection-external') AND gut_code = ANY(:gutCode) and user_id = :userId AND CAST(updated_at AS DATE) <= :dateUntil "
       + " order by updated_at desc OFFSET :offset LIMIT :limit")
   List<UserDomainPortfolioModel> fetchCompetencyCollections(
       @BindBean UserDomainPortfolioCommand.UserDomainPortfolioCommandBean userDomainPortfolioCommandBean,
@@ -44,14 +44,14 @@ interface UserDomainPortfolioDao {
   @Mapper(UserDomainPortfolioModelMapper.class)
   @SqlQuery("select gut_code, collection_id, latest_session_id, collection_score, collection_type, class_id, course_id, unit_id, lesson_id, content_source, created_at, updated_at "
       + " from learner_profile_competency_evidence where collection_type IN ('collection', 'collection-external') AND gut_code = ANY(:gutCode) and user_id = :userId "
-      + " AND updated_at BETWEEN :startDate AND :endDate order by updated_at desc OFFSET :offset LIMIT :limit")
+      + " AND CAST(updated_at AS DATE) BETWEEN :startDate AND :endDate order by updated_at desc OFFSET :offset LIMIT :limit")
   List<UserDomainPortfolioModel> fetchCompetencyCollectionsInDateRange(
       @BindBean UserDomainPortfolioCommand.UserDomainPortfolioCommandBean userDomainPortfolioCommandBean,
       @Bind("gutCode") PGArray<String> gutCode);
 
   @Mapper(UserDomainPortfolioModelMapper.class)
   @SqlQuery("select gut_code, collection_id, latest_session_id, collection_score, collection_type, class_id, course_id, unit_id, lesson_id, content_source, created_at, updated_at "
-      + " from learner_profile_competency_evidence where collection_type = 'offline-activity' AND gut_code = ANY(:gutCode) and user_id = :userId AND updated_at <= :dateUntil "
+      + " from learner_profile_competency_evidence where collection_type = 'offline-activity' AND gut_code = ANY(:gutCode) and user_id = :userId AND CAST(updated_at AS DATE) <= :dateUntil "
       + " order by updated_at desc OFFSET :offset LIMIT :limit")
   List<UserDomainPortfolioModel> fetchCompetencyOfflineActivities(
       @BindBean UserDomainPortfolioCommand.UserDomainPortfolioCommandBean userDomainPortfolioCommandBean,
@@ -60,7 +60,7 @@ interface UserDomainPortfolioDao {
   @Mapper(UserDomainPortfolioModelMapper.class)
   @SqlQuery("select gut_code, collection_id, latest_session_id, collection_score, collection_type, class_id, course_id, unit_id, lesson_id, content_source, created_at, updated_at "
       + " from learner_profile_competency_evidence where collection_type = 'offline-activity' AND gut_code = ANY(:gutCode) and user_id = :userId "
-      + " AND updated_at BETWEEN :startDate AND :endDate order by updated_at desc OFFSET :offset LIMIT :limit")
+      + " AND CAST(updated_at AS DATE) BETWEEN :startDate AND :endDate order by updated_at desc OFFSET :offset LIMIT :limit")
   List<UserDomainPortfolioModel> fetchCompetencyOfflineActivitiesInDateRange(
       @BindBean UserDomainPortfolioCommand.UserDomainPortfolioCommandBean userDomainPortfolioCommandBean,
       @Bind("gutCode") PGArray<String> gutCode);
